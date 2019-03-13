496ca2f521140ea094c4959b7593ba5f30e2323b
simbalinux55 <44467904+simbalinux55@users.noreply.github.com>
2019-03-13 20:24:39 +0200

    Add files via upload

---
+++ Code/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/font-awesome.css">
+
+</head>
+
+<body>
+
+<div id="mainheader">
+	
+	<h1>Silo Health Monitoring System</h1>
+	<img src="images\GMB123.bmp">
+	
+</div>	
+
+
+<section class="login">
+<form name="regfrom">
+<h1>LOG IN PAGE</h1>
+<div>
+<label for="name">Name</label><br>
+<input #ID="name "type="text" name="name" placeholder="Enter Name" required />
+</div>
+
+<div>
+<label for="password">Password</label><br>
+<input type="password" name="password" placeholder="Enter Password" required/>
+</div>
+
+ <button type="submit" name="login">Log In</button>
+</form>
+</section>
+
+<div class="news">
+		<p>
+			If you want to create an account or if you have forgotten your password, send an email with your details to the Administrator at the following email "admin@gmbdura.co.zw.
+		</p>
+</div>
+
+
+</body>
+
+</html>
